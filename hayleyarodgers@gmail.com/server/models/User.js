892c88101c560168df90dcc7de54d1a1251cf523
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-12-06 13:29:12 +1100

    Write User model

---
+++ hayleyarodgers@gmail.com/server/models/User.js
@@ -0,0 +1,59 @@
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+// Schema to create User model
+const userSchema = new Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+		// Use regex to validate email
+		match: [
+			/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+			"Please use a valid email address.",
+		],
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	// Role used to manage a user's permissions
+	// One-to-one relationshp
+	role: {
+		type: Schema.Types.ObjectId,
+		ref: "Role",
+	},
+	// Business the user is associated with
+	// One-to-one relationship
+	business: {
+		type: Schema.Types.ObjectId,
+		ref: "Business",
+	},
+});
+
+// Use bcrypt to hash user's password
+// This means the user's password is saved in the database in a different format to what it was entered to increase security
+userSchema.pre("save", async function (next) {
+	if (this.isNew || this.isModified("password")) {
+		const saltRounds = 10;
+		this.password = await bcrypt.hash(this.password, saltRounds);
+	}
+
+	next();
+});
+
+// Custom method to compare and validate password when user logs in
+userSchema.methods.isCorrectPassword = async function (password) {
+	return bcrypt.compare(password, this.password);
+};
+
+// Initialise User model
+const User = model("User", userSchema);
+
+module.exports = User;
