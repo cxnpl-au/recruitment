1d4b8da14477a1186554d149204e8b12e17e5d3f
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-12-07 10:23:55 +1100

    Write functions for saving, checking and deleting token needed for authorisation

---
+++ hayleyarodgers@gmail.com/client/src/utils/auth.js
@@ -0,0 +1,52 @@
+// Use to decode a token and get the user's information out of it
+import decode from "jwt-decode";
+
+// Create a new class to instantiate for a user
+class AuthService {
+  // Upon login, save the user's token to localStorage
+  login(idToken) {
+    localStorage.setItem("id_token", idToken);
+    window.location.assign("/");
+  }
+
+  // Retrieve the user's token from localStorage
+  getToken() {
+    return localStorage.getItem("id_token");
+  }
+
+  // Check if token is expired
+  isTokenExpired(token) {
+    try {
+      // Decode the token to get its expiration time (that was set by the server)
+      const decoded = decode(token);
+
+      // If the expiration time is less than the current time (in seconds) then the token is expired, return true
+      // If token hasn't passed its expiration time, return false
+      if (decoded.exp < Date.now() / 1000) {
+        localStorage.removeItem("id_token");
+        localStorage.removeItem("saved_user_role");
+        return true;
+      } else return false;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  // Check if the user is logged in
+  loggedIn() {
+    // Checks if there is a saved token
+    const token = this.getToken();
+
+    // If there is a token and it's not expired, return true
+    return !!token && !this.isTokenExpired(token) ? true : false;
+  }
+
+  // Upon logout, remove the user's token from localStorage
+  logout() {
+    localStorage.removeItem("id_token");
+    // Reload the page and reset the state of the application
+    window.location.assign("/");
+  }
+}
+
+export default new AuthService();
