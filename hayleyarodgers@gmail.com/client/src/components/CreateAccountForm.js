c1c8ab7ce165dcb693a25e0202b1ddd06ad86a80
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-12-07 20:20:46 +1100

    Write and style component for creating accounts under a business

---
+++ hayleyarodgers@gmail.com/client/src/components/CreateAccountForm.js
@@ -0,0 +1,123 @@
+import React, { useState } from "react";
+
+// Import bootstrap components
+import { Form, Button, Alert } from "react-bootstrap";
+
+// Import API call, authentication token and getting saved business Id from local storage functions
+import { createAccount } from "../utils/API";
+import Auth from "../utils/auth";
+import { getSavedBusinessId } from "../utils/localStorage";
+
+const AccountForm = ({ handleModalClose }) => {
+  // Set initial form state
+  const [accountFormData, setAccountFormData] = useState({
+    name: "",
+    balance: 0,
+  });
+
+  // Set state for form validation
+  const [validated] = useState(false);
+
+  // Set state for alert
+  const [showAlert, setShowAlert] = useState(false);
+
+  // When the input of any form field changes, set form data
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setAccountFormData({ ...accountFormData, [name]: value });
+  };
+
+  // Create new account
+  const handleFormSubmit = async (event) => {
+    // Stop page refresh
+    event.preventDefault();
+
+    // Check token before proceeding
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+
+    if (!token) {
+      return false;
+    }
+
+    // Check if form has everything (as per react-bootstrap docs)
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    // Since createAccount is asynchronous, wrap in a `try...catch` to catch any network errors from throwing due to a failed request
+    try {
+      const businessId = getSavedBusinessId();
+      const response = await createAccount(businessId, accountFormData, token);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong when creating account.");
+      }
+
+      // Reset form data
+      setAccountFormData({
+        name: "",
+        balance: 0,
+      });
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+  };
+
+  return (
+    <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+      {/* Show alert if server response is bad */}
+      <Alert
+        dismissible
+        onClose={() => setShowAlert(false)}
+        show={showAlert}
+        variant="danger">
+        Something went wrong.
+      </Alert>
+
+      {/* Account name input */}
+      <Form.Group className="mb-3">
+        <Form.Control
+          type="text"
+          placeholder="Name"
+          name="name"
+          onChange={handleInputChange}
+          value={accountFormData.name}
+          required
+        />
+        <Form.Control.Feedback type="invalid" muted>
+          Please add a name for your account.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      {/* Account balance input */}
+      <Form.Group className="mb-3">
+        <div className="input-group d-flex align-items-center">
+          <p>$</p>
+          <Form.Control
+            type="number"
+            placeholder="Account starting balance"
+            name="balance"
+            min="0.01"
+            step="0.01"
+            onChange={handleInputChange}
+            value={accountFormData.balance}
+            required
+          />
+        </div>
+        <Form.Control.Feedback type="invalid" muted>
+          Please add a starting balance for your account.
+        </Form.Control.Feedback>
+      </Form.Group>
+
+      {/* On form submit, create account */}
+      <Button type="submit" variant="success" onClick={handleModalClose}>
+        Create
+      </Button>
+    </Form>
+  );
+};
+
+export default AccountForm;
--- hayleyarodgers@gmail.com/client/src/pages/Dashboard.js
+++ hayleyarodgers@gmail.com/client/src/pages/Dashboard.js
@@ -4,13 +4,45 @@
 // Viewers can view all accounts
 // User must be signed in to view this page
 
-import React from "react";
+import React, { useState } from "react";
+
+// Import bootstrap components
+import { Tab, Modal, Button } from "react-bootstrap";
+
+// Import components
+import AccountForm from "../components/CreateAccountForm";
 
 const Dashboard = () => {
+  // Set modal display state
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <main>
       {/* Page title */}
-      <h2>Dashboard</h2>
+      <div className="d-flex align-items-center mb-3">
+        <h2>Accounts</h2>
+        <Button className="btn btn-link" onClick={() => setShowModal(true)}>
+          +
+        </Button>
+      </div>
+
+      {/* Modal to create new account */}
+      <Modal
+        centered
+        className="d-flex justify-content-center"
+        show={showModal}
+        onHide={() => setShowModal(false)}
+        aria-labelledby="create-account-modal">
+        {/* Tab container to show either signup or login component */}
+        <Tab.Container>
+          <Modal.Header closeButton>
+            <h3>Create an account</h3>
+          </Modal.Header>
+          <Modal.Body>
+            <AccountForm handleModalClose={() => setShowModal(false)} />
+          </Modal.Body>
+        </Tab.Container>
+      </Modal>
     </main>
   );
 };
