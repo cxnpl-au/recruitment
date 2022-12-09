77c914031ac5618d8a1bf449e3253ee59f03b912
hayleyadele <101703489+hayleyarodgers@users.noreply.github.com>
2022-12-09 15:25:03 +1100

    Add comments

--- hayleyarodgers@gmail.com/server/utils/resetPassword.js
+++ hayleyarodgers@gmail.com/server/utils/resetPassword.js
@@ -12,12 +12,15 @@ require("dotenv").config({ path: path.resolve(__dirname, "../../.env") });
 const clientURL = process.env.CLIENT_URL;
 
 const requestPasswordReset = async (email) => {
+	// Find user based on email entered
 	const user = await User.findOne({ email });
 	if (!user) throw new Error("No user found with that email.");
 
+	// Delete any existing reset password tokens associated with user
 	let token = await ResetPasswordToken.findOne({ userId: user._id });
 	if (token) await token.deleteOne();
 
+	// Create new hashed reset password token
 	let resetToken = crypto.randomBytes(32).toString("hex");
 	const hash = await bcrypt.hash(resetToken, Number(10));
 
@@ -29,12 +32,14 @@ const requestPasswordReset = async (email) => {
 
 	const link = `${clientURL}/passwordreset?token=${resetToken}&id=${user._id}`;
 
+	// Send reset password email with link to reset password
 	resetPasswordEmail(user.email, user.username, link);
 
 	return link;
 };
 
 const resetPassword = async (userId, token, password) => {
+	// Find reset password token associated with user and check if valid
 	let resetPasswordToken = await ResetPasswordToken.findOne({ userId });
 
 	if (!resetPasswordToken) {
@@ -47,14 +52,17 @@ const resetPassword = async (userId, token, password) => {
 		throw new Error("Invalid or expired password reset token.");
 	}
 
+	// If reset password token is valid, hash new password
 	const hash = await bcrypt.hash(password, Number(10));
 
+	// Save new password to user
 	await User.updateOne(
 		{ _id: userId },
 		{ $set: { password: hash } },
 		{ new: true }
 	);
 
+	// Delete reset password token
 	await resetPasswordToken.deleteOne();
 
 	return true;
