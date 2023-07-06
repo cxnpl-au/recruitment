800df70ebdea1922e13b1d972e720b3e5985ad97
Usman Zahid <usmanzahid1998@gmail.com>
2023-07-06 14:41:55 +1000

    Completed logic for some ORG operations

--- lambda-uz-cnxpl/lambda_function.py
+++ lambda-uz-cnxpl/lambda_function.py
@@ -1,5 +1,9 @@
+import boto3
 from enum import Enum
+import hashlib
 import json
+import random
+import time
 
 from custom_encoder import CustomEncoder
 from organisations import Organisations
@@ -11,36 +15,34 @@ from organisations import Organisations
 # ORGANISATION
 # ------------
 # {
-#   alias: string,
-#   name: string,
+#   alias: string, <HK>
+#   name: string, <SK>
 #   account_limit: number, (default 20)
 #   user_limit: number, (default 2000)
 #   auth_timeout_secs: number, (default 600)
 #   accounts: [
 #     {
 #       name: string,
-#       amount: number,
-#       admins: [string], <user_alias>
-#       modifiers: [string], <user_alias>
-#       viewers: [number] <user_alias>
-#     },
-#     ...
-#   ],
-#   users: [
-#     {
-#       alias: string, <user-name>
-#       authenticated: {
-#         ip_address: string, <hashed-value>
-#         logged_out: boolean,
-#         token: string, <base64-encoded-key>
-#         time: number <seconds from UNIX epoch>
-#       },
-#       name: string,
-#       password: string, <hashed-value>
-#       role: string, <one of 'admin', 'acc_manager', 'normal'> # Root will be a special admin account
+#       amount: number
 #     },
 #     ...
 #   ]
+# }
+#
+# USERS
+# -----
+# {
+#   org: string, <HK>
+#   alias: string, <SK>
+#   name: string,
+#   password: string, <hashed-value>
+#   role: string, <one of 'account_manager', 'admin', 'normal'> # Root will be a special admin account
+#   authentication: {
+#     ip_address: string, <hashed-value>,
+#     logged_out: boolean,
+#     token: string, <hashed-random-base64-key>
+#     time: number <seconds from UNIX epoch>
+#   }
 # }
 
 # API Specification
@@ -55,24 +57,21 @@ from organisations import Organisations
 #       }
 #     - Response
 #       {
-#         id: number,
-#         name: string,
 #         token: string
 #       }
 #   - GET
 #     - Request
 #       {
-#         name: string, <must be 'root'>
+#         alias: string,
+#         name: string,
 #         password: string
 #       }
 #     - Response
 #       {
-#         token: string,
-#         id: number,
 #         name: string,
-#         account_limit: number,
-#         user_limit: number,
-#         auth_timeout_secs: number
+#         account_limit: number, <if root>
+#         user_limit: number, <if root>
+#         auth_timeout_secs: number <if root>
 #       }
 #   - PATCH
 #     - Request
@@ -229,14 +228,193 @@ from organisations import Organisations
 # DYNAMODB = boto3.resource("dynamodb")
 # TABLE = DYNAMODB.Table(TABLE_NAME)
 
+DYNAMODB = boto3.resource("dynamodb")
+
+
+class DefaultLimits(Enum):
+    ACCOUNTS = 20
+    AUTH_TIMEOUT_SECS = 600
+    USERS = 2000
+
+
+class Roles(Enum):
+    ADMIN = 0
+    ACCOUNT_MANAGER = 1
+    NORMAL = 2
+
+
+class Authentication:
+    ip: str
+    logged_out: bool
+    token: str
+    time: float
+
+    def __init__(self, ip: str) -> None:
+        self.ip = hashlib.sha256(ip.encode("utf-8")).hexdigest()
+        self.logged_out = False
+        self.token = f"{random.randrange(2 ** 256):#064x}"
+        self.time = time.time()
+
+    @classmethod
+    def from_existing(
+        cls, ip: str, logged_out: bool, token: str, time: float
+    ) -> "Authentication":
+        cls.ip = ip
+        cls.logged_out = logged_out
+        cls.token = token
+        cls.time = time
+
+        return cls
+
+
+class Organisation:
+    account_limit: int = DefaultLimits.ACCOUNTS
+    accounts = []
+    alias: str
+    auth_timeout_secs: int = DefaultLimits.AUTH_TIMEOUT_SECS
+    name: str
+    user_limit: int = DefaultLimits.USERS
+
+    def __init__(self, alias: str, name: str) -> None:
+        self.alias = alias
+        self.name = name
+
+
+class User:
+    alias: str
+    auth: Authentication
+    name: str
+    org: str
+    password: str
+    role: Roles
+
+    def __init__(
+        self, alias: str, ip: str, name: str, org: str, password: str, role: Roles
+    ) -> None:
+        self.alias = alias
+        self.auth = Authentication(ip)
+        self.name = name
+        self.org = org
+        self.password = hash_password(org, alias, password)
+        self.role = role
+
+    @classmethod
+    def from_existing(
+        cls,
+        alias: str,
+        auth: Authentication,
+        name: str,
+        org: str,
+        password: str,
+        role: Roles,
+    ) -> "User":
+        cls.alias = alias
+        cls.auth = auth
+        cls.name = name
+        cls.org = org
+        cls.password = password
+        cls.role = role
+
+
+# TODO: Orgs Table
+class OrgsTable:
+    # TABLE = DYNAMODB.Table("organisations")
+
+    def insert(self, org: Organisation) -> bool:
+        return False
+
+
+# TODO: Users Table
+class UsersTable:
+    # TABLE = DYNAMODB.Table("users")
+
+    def insert(self, user: User) -> bool:
+        return False
+
+
+ORGS = OrgsTable()
+USERS = UsersTable()
+
 
 def lambda_handler(event, context) -> dict[str, any]:
-    code = 400
-    body = None
-
     match event["path"]:
         case "/organisations":
             (code, body) = organisations(event, context)
+        case _:
+            (code, body) = (400, None)
+
+    return create_response(code, body)
+
+
+# TODO: Implement remaining stubs
+def organisations(event, context) -> tuple[int, dict[str, any]]:
+    method = event["httpMethod"]
+    ip = context["identity"]["sourceIp"]
+
+    match method:
+        case "POST":
+            body = json.loads(event["body"])
+
+            alias = body["alias"]
+            name = body["name"]
+            password = body["password"]
+
+            response = create_organisation(alias, name, password, ip)
+        case "GET":
+            body = json.loads(event["body"])
+
+            alias = body["alias"]
+            name = body["name"]
+            password = body["password"]
+
+            response = get_organisation(alias, name, password)
+        case "PATCH":
+            body = json.loads(event["body"])
+
+            pass
+        case "DELETE":
+            pass
+
+    return response
+
+
+def create_organisation(
+    alias: str, name: str, password: str, ip: str
+) -> dict[str, any]:
+    org = Organisation(alias, name)
+
+    if OrgsTable().insert(org):
+        user = User("root", ip, "root", alias, password, Roles.ADMIN)
+        UsersTable.insert(user)
+
+        code = 200
+        body = {"token": user.authentication.token}
+    else:
+        code = 400
+        body = None
+
+    return create_response(code, body)
+
+
+# TODO: Finish table functions
+def get_organisation(alias: str, name: str, password: str, ip: str) -> dict[str, any]:
+    code = 400
+    body = None
+
+    if ORGS.exists(alias):
+        if USERS.authenticate(name, password, ip):
+            if alias == "root":
+                org = ORGS.get(alias)
+                body = {
+                    "account_limit": org.account_limit,
+                    "auth_timeout_secs": org.auth_timeout_secs,
+                    "name": org.name,
+                    "user_limit": org.user_limit,
+                }
+            else:
+                body = {"name": ORGS.name(alias)}
+
+            code = 200
 
     return create_response(code, body)
 
@@ -256,8 +434,20 @@ def create_response(status: int, body=None) -> dict[str, any]:
     return response
 
 
-def organisations(event, context) -> tuple[int, dict[str, any]]:
-    pass
+def hash_password(org: str, alias: str, password: str) -> str:
+    # TODO: Use multiple rounds of hashing with pre-generated random numbers to extend the key
+
+    # org + alias is guaranteed to be unique for each user
+    # We apply a two-layer salt to the passwords
+    first = hashlib.sha256(org.encode("utf-8")).hexdigest().encode("utf-8")
+    second = hashlib.sha256(first)
+    second.update(alias.encode("utf-8"))
+
+    salt = second.hexdigest().encode("utf-8")
+    h = hashlib.sha256(salt)
+    h.update(password.encode("utf-8"))
+
+    return h.hexdigest()
 
 
 # class QueryType(Enum):
